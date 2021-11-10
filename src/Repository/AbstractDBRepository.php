0cbdb8afa5b93054ef177b31add8f33b56270fe8
Tariq Ayman <tariq.ayman94@gmail.com>
2021-11-10 20:17:59 +0200

    create Abstract Repository

---
+++ src/Repository/AbstractDBRepository.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * Created by PhpStorm.
+ * Developer: Tariq Ayman ( tariq.ayman94@gmail.com )
+ * Date: 11/10/21, 7:51 PM
+ * Last Modified: 11/9/21, 2:06 PM
+ * Project Name: aqarmap-blog-task
+ * File Name: AbstractDBRepository.php
+ */
+
+namespace App\Repository;
+
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+
+/**
+ * @package App\Repository
+ */
+abstract class AbstractDBRepository extends ServiceEntityRepository
+{
+}
