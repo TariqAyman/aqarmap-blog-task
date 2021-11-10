41374277a908f423ce367f63da3c7005b65103a3
Tariq Ayman <tariq.ayman94@gmail.com>
2021-11-10 20:16:31 +0200

    create category entity

---
+++ src/Entity/Category.php
@@ -0,0 +1,49 @@
+<?php
+/*
+ * Created by PhpStorm.
+ * Developer: Tariq Ayman ( tariq.ayman94@gmail.com )
+ * Date: 11/10/21, 7:48 PM
+ * Last Modified: 11/9/21, 1:59 PM
+ * Project Name: aqarmap-blog-task
+ * File Name: Category.php
+ */
+
+namespace App\Entity;
+
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CategoryRepository::class)
+ */
+class Category extends AbstractEntity
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $name;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
