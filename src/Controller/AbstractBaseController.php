4f08808890a77b88fb4daec919fe06b417948fa7
Tariq Ayman <tariq.ayman94@gmail.com>
2021-11-10 20:17:45 +0200

    create Abstract controller

---
+++ src/Controller/AbstractBaseController.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * Created by PhpStorm.
+ * Developer: Tariq Ayman ( tariq.ayman94@gmail.com )
+ * Date: 11/10/21, 7:34 PM
+ * Last Modified: 11/9/21, 2:31 PM
+ * Project Name: aqarmap-blog-task
+ * File Name: AbstractBaseController.php
+ */
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+/**
+ * @package App\Controller\Abstracts
+ */
+abstract class AbstractBaseController extends AbstractController
+{
+}
+
