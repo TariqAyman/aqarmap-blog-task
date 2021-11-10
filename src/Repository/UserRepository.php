cf72472b83e51ccab618c80b9e032626c5ec74da
Tariq Ayman <tariq.ayman94@gmail.com>
2021-11-10 21:27:26 +0200

    create UserRepository

---
+++ src/Repository/UserRepository.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * Created by PhpStorm.
+ * Developer: Tariq Ayman ( tariq.ayman94@gmail.com )
+ * Date: 11/10/21, 7:52 PM
+ * Last Modified: 11/9/21, 2:07 PM
+ * Project Name: aqarmap-blog-task
+ * File Name: UserRepository.php
+ */
+
+namespace App\Repository;
+
+use App\Entity\User;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method User|null find($id, $lockMode = null, $lockVersion = null)
+ * @method User|null findOneBy(array $criteria, array $orderBy = null)
+ * @method User[]    findAll()
+ * @method User[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class UserRepository extends AbstractDBRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, User::class);
+    }
+}
