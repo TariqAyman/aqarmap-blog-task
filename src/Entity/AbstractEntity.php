273d889d4182aeb6d3aaa64d7a3d8a754a5919ca
Tariq Ayman <tariq.ayman94@gmail.com>
2021-11-10 20:17:23 +0200

    create AbstractEntity

---
+++ src/Entity/AbstractEntity.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Created by PhpStorm.
+ * Developer: Tariq Ayman ( tariq.ayman94@gmail.com )
+ * Date: 11/10/21, 7:40 PM
+ * Last Modified: 11/9/21, 1:58 PM
+ * Project Name: aqarmap-blog-task
+ * File Name: AbstractEntity.php
+ */
+
+namespace App\Entity;
+
+abstract class AbstractEntity
+{
+
+}
